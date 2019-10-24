eca763f1546ecb218810c1686ecab80b5453544f
chew <inferno.chew@gmail.com>
2019-10-24 16:33:25 +0900

    新しいプロジェクト作成

---
+++ Siew Bin/New Unity Project/Assets/Script/PlayerMng.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMng : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.localPosition += new Vector3(0.0f,0.0f,1.0f);
+        }
+    }
+}
