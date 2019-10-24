276dd5b7f3fe475e0ba23689fdaa84c54b7ae4db
Siew Bin <tekito@kk.kk>
2019-10-25 02:14:45 +0900

    プレイヤー基本の動きとカメラの設定

---
+++ Siew Bin/Assets/Script/PlayerMng.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMng : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+           
+            transform.localPosition += new Vector3(0.1f,0.0f,0.0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.localPosition += new Vector3(-0.1f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.localPosition += new Vector3(0.0f, 0.0f, 0.1f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.localPosition += new Vector3(0.0f, 0.0f, -0.1f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+            
+            rb.AddForce(new Vector3(0.0f, 5.0f, 0.0f),ForceMode.Impulse);
+
+        }
+    }
+}
