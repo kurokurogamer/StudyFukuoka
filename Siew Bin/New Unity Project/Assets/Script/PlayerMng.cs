b72799cbe2c944ea6f3f7fad875dfaa4cd663634
chew <inferno.chew@gmail.com>
2019-10-25 18:30:18 +0900

    モデル、アニメーションとクロスヘッドを入れました。

--- Siew Bin/New Unity Project/Assets/Script/PlayerMng.cs
+++ Siew Bin/New Unity Project/Assets/Script/PlayerMng.cs
@@ -2,20 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMng : MonoBehaviour
 {
+    float distToGround;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("right"))
+        bool IsMoving = false;
+        if (Input.GetKey("d"))
         {
-            transform.localPosition += new Vector3(0.0f,0.0f,1.0f);
+            SetSpeed(1.0f);
+            transform.localPosition += new Vector3(0.1f,0.0f,0.0f);
+            IsMoving = true;
+        }
+        if (Input.GetKey("a"))
+        {
+            SetSpeed(1.0f);
+            transform.localPosition += new Vector3(-0.1f, 0.0f, 0.0f);
+            IsMoving = true;
+        }
+        if (Input.GetKey("w"))
+        {
+            SetSpeed(1.0f);
+            transform.localPosition += new Vector3(0.0f, 0.0f, 0.1f);
+            IsMoving = true;
+
+        }
+        if (Input.GetKey("s"))
+        {
+            SetSpeed(1.0f);
+            transform.localPosition += new Vector3(0.0f, 0.0f, -0.1f);
+            IsMoving = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&& IsGrounded())
+        {
+            Debug.Log("Jump");
+            //GetComponent<Animator>().SetBool("Jump", true);
+            rb.AddForce(new Vector3(0.0f, 5.0f, 0.0f),ForceMode.Impulse);
+
+        }
+        if (IsGrounded())
+        {
+            GetComponent<Animator>().SetTrigger("OnGround");
+            GetComponent<Animator>().SetBool("Jump", false);
+
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Jump", true);
+        }
+        if (!IsMoving)
+        {
+            SetSpeed(0.0f);
         }
     }
+
+
+    void SetSpeed(float val)
+    {
+        GetComponent<Animator>().SetFloat("Speed", val);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+
 }
