b8a7da4c915b0233a183eae3a1f697175909b454
白枝友紀 <kurokurogamer42@gmail.com>
2019-10-25 16:52:42 +0900

    エフェクトテスト用のボタンを追加しました。 エフェクトスクリプトを追加しました。 Createで通常生成 CreatePoolでプールの中からオブジェクトを使いまわしてしようします。

---
+++ Shiraeda/TestProject/Assets/Script/TestButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    Effect effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effect = GetComponent<Effect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            effect.CreatePool(0, transform.position, transform.rotation, true, true);
+        }
+        else if(Input.GetKeyDown(KeyCode.X))
+        {
+            effect.Play(0);
+        }
+        else if(Input.GetKeyDown(KeyCode.Y))
+        {
+            effect.Stop(0);
+        }
+    }
+}
