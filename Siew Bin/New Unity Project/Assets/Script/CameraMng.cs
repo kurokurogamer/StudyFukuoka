d0ab45f384485678598968ff67719a9e729ca52f
chew <inferno.chew@gmail.com>
2019-10-25 18:39:47 +0900

    モデルとアニメーションを入れました。
    画面にクロスヘッドを入れました。

---
+++ Siew Bin/New Unity Project/Assets/Script/CameraMng.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMng : MonoBehaviour
+{
+    private float hSpeed = 2.0f;
+    private float vSpeed = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+  [SerializeField]
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x,
+     player.transform.position.y +1.5f,
+     player.transform.position.z+0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += hSpeed * Input.GetAxis("Mouse X");
+        pitch -= vSpeed * Input.GetAxis("Mouse Y");
+        transform.position = new Vector3(player.transform.position.x, 
+            player.transform.position.y +1.5f, 
+            player.transform.position.z+0.2f);
+        transform.eulerAngles= new Vector3(pitch, yaw, 0);
+        
+    }
+
+}
