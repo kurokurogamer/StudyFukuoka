a28cdafcf8c81d20b873cbe8b43a23699f9fcf0a
白枝友紀 <kurokurogamer42@gmail.com>
2019-10-25 19:45:33 +0900

    コミット

---
+++ Shiraeda/TestProject/Assets/Script/Fade.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fade : MonoBehaviour
+{
+    // フェイド用イメージ保存変数
+    Image _image;
+    Color _color;
+    // RGB値
+    float r, g, b;
+    // アルファ値
+    float _alpha;
+
+    // フェイド速度(1秒単位)
+    [SerializeField, Tooltip("フェイド速度(1秒単位)")]
+    float _FadeFlamTime = 5;
+    // テストフラグ
+    bool _isFade = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // フェイド用のイメージを取得
+        _image = GetComponent<Image>();
+        // 初期色を黒に指定
+        _color = _image.color;
+        // 透明度指定(不透明)
+        _alpha = 1;
+    }
+
+    public void FadeIn()
+    {
+        _alpha -= Time.deltaTime;
+        SetAlpha();
+        // 透明度が0以下なら
+        if (_alpha <= 0)
+        {
+            _image.enabled = false;
+            _alpha = 0;
+            _isFade = true;
+        }
+    }
+
+    public void FadeOut()
+    {
+        _alpha += Time.deltaTime;
+        _image.enabled = true;
+        // アルファ値が不透明の255を超えているなら
+        if (_alpha >= 1)
+        {
+            _alpha = 1;
+            _isFade = false;
+        }
+    }
+
+    private void SetAlpha()
+    {
+        _image.color = new Color(_color.r, _color.g, _color.b,_alpha);
+    }
+
+    private void StartFade()
+    {
+        if(_isFade)
+        {
+            FadeOut();
+        }
+        else
+        {
+            FadeIn();
+        }
+        SetAlpha();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartFade();
+    }
+}
--- Shiraeda/TestProject/Assets/Script/SceneCtl.cs
+++ Shiraeda/TestProject/Assets/Script/SceneCtl.cs
@@ -1,25 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneCtl : MonoBehaviour
 {
-    static SceneCtl instance = null;
+    private static SceneCtl instance = null;
+    string _sceneName = "";
     public static SceneCtl Instance
     {
         get
         {
+            if (instance == null)
+            {
+                GameObject obj = new GameObject();
+                obj.name = "AudioManager";
+                instance = obj.AddComponent<SceneCtl>();
+            }
             return instance;
         }
     }
     private void Awake()
     {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
         DontDestroyOnLoad(this);
     }
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void MoveScene()
+    {
+
+    }
+
+    private void CheckScene()
+    {
+        // ActiveなSceneの名前を取得
+        _sceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
